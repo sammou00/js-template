b0711aef8d6641866e8c7b68b77e2d7be5c1b48b
Agnieszka-Dzwolak <dzwolak.agnieszka@gmail.com>
2024-05-06 13:46:52 +0200

    add coffee

--- src/handlers/clearAllHandler.js
+++ src/handlers/clearAllHandler.js
@@ -4,22 +4,21 @@ import dom from '../dom.js';
 const clearAllHandler = () => {
     //dom
     if (confirm('Are you sure you want to clear all the items?')) {
+        //data
+        data.items = [];
         dom.itemsList.innerHTML = '';
         alert('Items cleared successfully');
 
-        //         if (data.items.length === 0) {
-        //             dom.itemsList.innerHTML = `Now you can rest and watch Netflix
+        if (data.items.length === 0) {
+            dom.itemsList.innerHTML = `<p id="coffee">Now you can rest and watch Netflix
 
-        //         <span class="material-symbols-outlined">
-        // coffee
-        // </span>`;
-        //         }
+                <span class="material-symbols-outlined">
+        coffee
+        </span></p>`;
+        }
     } else {
         alert('Clearing cancelled');
     }
-
-    //data
-    data.items = [];
 };
 
 export default clearAllHandler;
